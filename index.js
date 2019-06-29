147d09807f84b437907f719a932b3fbd0e091e52
jienco <jonatan.ienco@gmail.com>
2019-06-29 18:45:40 -0300

    Working version

--- index.js
+++ index.js
@@ -2,14 +2,31 @@
 import './style.css';
 import videojs from 'video.js';
 
-var options = {};
+var options = {
+  controls: true,
+  autoplay: true,
+  preload: 'auto',
+  liveui: true,
+};
+var isPlayerReady = false;
 
 var player = videojs('videoPlayer', options, function onPlayerReady() {
   videojs.log('Player is ready!');
+  isPlayerReady = true;
 });
 
 function play() {
-  var inputUrlTextElement = document.getElementById('input-url');
-  var url = inputUrlTextElement.innerText;
-  alert(url);
+  if (isPlayerReady === false) {
+    alert('The player is not ready yet.');
+    return;
+  }
+  var urlInputElement = document.getElementById('urlInput');
+  var url = urlInputElement.value;
+  var contentType = 'video/mp4';
+  if (url.includes('.m3u8')) {
+    contentType = 'application/x-mpegURL';
+  }
+  player.src({type: contentType, src: url});
 }
+
+document.getElementById('playButton').addEventListener("click", play);
